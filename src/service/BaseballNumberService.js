67cf279abbb33d250bd29c21ca726c09684056c8
SeongUk52 <rkfka7849@gmail.com>
2026-01-03 16:48:55 +0900

    feat: 컴퓨터의 야구숫자 랜덤 생성 기능

---
+++ src/service/BaseballNumberService.js
@@ -0,0 +1,23 @@
+import {MissionUtils} from "@woowacourse/mission-utils/src";
+import BaseballNumber from "../domain/BaseballNumber.js";
+
+class BaseballNumberService {
+    #computerBallNumber;
+
+    constructor() {
+        this.#computerBallNumber = new BaseballNumber(this.#randomGenerateBallNumber());
+    }
+
+    #randomGenerateBallNumber() {
+        const computer = [];
+        while (computer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!computer.includes(number)) {
+                computer.push(number);
+            }
+        }
+        return computer;
+    }
+}
+
+export default BaseballNumberService;
